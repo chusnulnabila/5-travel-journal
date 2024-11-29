84fecd49a21172ef420ea8df09e692d65207163f
chusnul nabila <chusnulnab@gmail.com>
2024-11-29 21:59:41 +0700

    to be continued

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Entry from "./component/Entry";
 import Header from "./component/Header";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Entry />
     </>
   );
 }
